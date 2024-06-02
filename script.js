6bedf85fdedec95889655f5623aa4f82630d6e94
ImShad0w <kirill19042006@gmail.com>
2024-06-03 00:18:59 +0200

    Added operators and more eventlisteners

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-let firstNum;
-let secondNum;
+
+//Initialize the variables;
+let firstNum = "";
+let currentNumo = "";
 let operator;
-let displayValue = 0;
+let displayValue = "";
+
+//Operater function based on operator
 function operate(operator, firstNum, secondNum) {
   let result;
   switch (operator) {
@@ -21,33 +25,41 @@ function operate(operator, firstNum, secondNum) {
   return result;
 }
 
-const add = function(firstNum, secondNum) {
+//functions that do the math
+const add = function (firstNum, secondNum) {
 
   let result = firstNum + secondNum;
   return result;
 }
 
-const substract = function(firstNum, secondNum) {
+const substract = function (firstNum, secondNum) {
   let subs = firstNum - secondNum;
   return subs;
 }
 
-const multiply = function(firstNum, secondNum) {
+const multiply = function (firstNum, secondNum) {
   let mult = firstNum * secondNum;
   return mult;
 }
 
-const divide = function(firstNum, secondNum) {
+const divide = function (firstNum, secondNum) {
   let div = firstNum / secondNum;
   return div;
 }
+//We select all of the numbers
 const numbers = document.querySelectorAll(".number")
-
+//For each we select the display and display the current number
 numbers.forEach(button => {
-  button.addEventListener('click', function() {
+  button.addEventListener('click', function () {
     const display = document.getElementById("display")
-    displayValue = button.innerHTML
-    display.innerHTML = displayValue;
+    displayValue = button.textContent
+    display.innerHTML += displayValue;
   })
 })
+
+const operators = document.querySelectorAll(".operator")
+operators.forEach((op) => op.addEventListener("click", function () {
+  operator = op.textContent;
+  console.log(operator);
+}))
 
